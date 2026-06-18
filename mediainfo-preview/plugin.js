e34ba2ecbf6ea44b3b9370e588525c0b4ebeaed3
Alexey <tvchelyabinsk@gmail.com>
2026-06-18 14:36:16 +0500

    Update plugin.js

--- mediainfo-preview/plugin.js
+++ mediainfo-preview/plugin.js
@@ -6,46 +6,24 @@
 
     console.log('[MediaInfo Preview] started');
 
-    Lampa.Listener.follow('full', function (e) {
+    setTimeout(function () {
 
-        if (e.type !== 'complete') return;
+        console.log('full-start', $('.full-start').length);
+        console.log('full-start__description', $('.full-start__description').length);
 
-        console.log('[FULL COMPLETE]', e);
+        $('body').append(
+            '<div style="' +
+            'position:fixed;' +
+            'top:100px;' +
+            'right:20px;' +
+            'z-index:999999;' +
+            'background:red;' +
+            'padding:20px;' +
+            'color:white;' +
+            'font-size:20px;' +
+            '">MEDIAINFO TEST</div>'
+        );
 
-        setTimeout(function () {
-
-            $('.mediainfo-preview-test').remove();
-
-            var block = $(
-                '<div class="mediainfo-preview-test" style="' +
-                'margin-top:1.5em;' +
-                'padding:1em;' +
-                'background:rgba(255,255,255,0.08);' +
-                'border-radius:0.5em;' +
-                'font-size:1.1em;' +
-                '">' +
-                '<b>MediaInfo Preview</b><br>' +
-                'TMDB ID: ' + (e.object ? e.object.id : 'unknown') +
-                '</div>'
-            );
-
-            console.log('DESCRIPTION', $('.full-start__description').length);
-            console.log('BODY', $('.full-start').length);
-
-            if ($('.full-start__description').length) {
-                $('.full-start__description').after(block);
-                console.log('INSERT AFTER DESCRIPTION');
-            }
-            else if ($('.full-start').length) {
-                $('.full-start').append(block);
-                console.log('INSERT INTO FULL');
-            }
-            else {
-                $('body').append(block);
-                console.log('INSERT INTO BODY');
-            }
-
-        }, 1000);
-    });
+    }, 3000);
 
 })();
