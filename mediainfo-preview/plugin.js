4b7b9e38cfc580803a72d194bfad0ac241803190
Alexey <tvchelyabinsk@gmail.com>
2026-06-18 14:08:27 +0500

    Update plugin.js

--- mediainfo-preview/plugin.js
+++ mediainfo-preview/plugin.js
@@ -1,38 +1,8 @@
 (function () {
     'use strict';
 
-    function init() {
-        Lampa.Noty.show('MediaInfo Preview Debug загружен');
+    alert('PLUGIN LOADED');
 
-        console.log('[MediaInfo Preview] plugin started');
-
-        Lampa.Listener.follow('full', function (e) {
-            console.log('[FULL EVENT]', e);
-
-            try {
-                Lampa.Noty.show('FULL: ' + (e.data?.movie?.title || e.data?.movie?.name || 'unknown'));
-            }
-            catch (err) {
-                console.log(err);
-            }
-        });
-
-        Lampa.Listener.follow('activity', function (e) {
-            console.log('[ACTIVITY EVENT]', e);
-        });
-
-        Lampa.Listener.follow('torrent_file', function (e) {
-            console.log('[TORRENT FILE EVENT]', e);
-        });
-    }
-
-    if (window.lampa_started) {
-        init();
-    }
-    else {
-        Lampa.Listener.follow('app', function (e) {
-            if (e.type === 'ready') init();
-        });
-    }
+    console.log('PLUGIN LOADED');
 
 })();
