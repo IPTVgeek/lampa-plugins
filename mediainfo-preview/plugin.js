ea680af73b363a45bc63c214ab6497293c70da33
Alexey <tvchelyabinsk@gmail.com>
2026-06-18 13:23:30 +0500

    Update plugin.js

--- mediainfo-preview/plugin.js
+++ mediainfo-preview/plugin.js
@@ -1,5 +1,13 @@
 (function () {
     'use strict';
 
-    console.log('MediaInfo Preview plugin loaded');
+    if (window.MediaInfoPreviewPlugin) return;
+    window.MediaInfoPreviewPlugin = true;
+
+    Lampa.Manifest = Lampa.Manifest || {};
+    Lampa.Manifest.plugins = Lampa.Manifest.plugins || {};
+
+    console.log('MediaInfo Preview loaded');
+
+    Lampa.Noty.show('MediaInfo Preview загружен');
 })();
