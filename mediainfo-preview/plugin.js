f888c6d230f0bd557251c0fbce186d55f4bc273c
Alexey <tvchelyabinsk@gmail.com>
2026-06-18 14:20:49 +0500

    Update plugin.js

--- mediainfo-preview/plugin.js
+++ mediainfo-preview/plugin.js
@@ -4,24 +4,11 @@
     console.log('[MediaInfo] Debug started');
 
     Lampa.Listener.follow('activity', function (e) {
-        console.log('[ACTIVITY FULL]', JSON.stringify(e, null, 2));
+        console.log('[ACTIVITY]', e);
     });
 
     Lampa.Listener.follow('full', function (e) {
-        console.log('[FULL EVENT]', e);
-
-        if (e.object) {
-            console.log('[FULL OBJECT]', e.object);
-        }
-
-        if (e.data) {
-            console.log('[FULL DATA]', e.data);
-        }
-
-        if (e.card) {
-            console.log('[FULL CARD]', e.card);
-        }
+        console.log('[FULL]', e);
     });
 
 })();
- 
